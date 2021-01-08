f1a62af5ee3d8bd3791638e9c3fe35c8aadfe044
Eduardo Fortuna <eduardofortuna99@gmail.com>
2021-01-07 20:19:47 -0600

    Error con los colores de los inputs

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 /*** VARIABLES ***/
 const btnEnviar = document.querySelector('#enviar');
 const formulario = document.querySelector('#enviar-mail');
-
+const btnReset = document.querySelector('#resetBtn');
+//Expresión regular
 const er = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
 //Variables para campos
@@ -12,6 +13,23 @@ const mensaje = document.querySelector('#mensaje');
 /*** lISTENERS ***/
 eventListeners();
 
+/** REPARAR ESTE MIARDA, EL PROBLEMA ES QUE YA NO APARECEN LOS COLORES**/
+
+/*** OBJETO COLORES INPUTS***/
+const coloresInput = {
+    verdeActivado : (e) => {
+        e.target.classList.add('border', 'border-green-500');
+    },
+    verdeDesactivado : e => {
+        e.target.classList.remove('border', 'border-green-500');
+    },
+    rojoActivado : e => {
+        e.target.classList.add('border', 'border-red-500');
+    },
+    rojoDesactivado : e =>{
+        e.target.classList.remove('border', 'border-red-500');
+    }
+}
 function eventListeners(){
     //Cuando la APP arranca
     document.addEventListener('DOMContentLoaded', iniciarApp);
@@ -20,6 +38,10 @@ function eventListeners(){
     email.addEventListener('blur', validarFormulario);
     asunto.addEventListener('blur', validarFormulario);
     mensaje.addEventListener('blur', validarFormulario);
+
+    //Botones
+    btnEnviar.addEventListener('click', enviarMensaje);
+    btnReset.addEventListener('click', resetearFormulario);
 }
 
 /*** FUNCIONES ***/
@@ -57,8 +79,8 @@ function validarFormulario(e){
 
 //En caso que lo que haya sido colocado sea correcto
 function mostrarCorrecto(e){
-    e.target.classList.remove('border', 'border-red-500');
-    e.target.classList.add('border', 'border-green-500');
+    coloresInput.rojoDesactivado(e);
+    coloresInput.verdeActivado(e);
     if(document.querySelectorAll('.error').length > 0){
         const errorMessage = document.querySelector('.error');
         errorMessage.remove();
@@ -66,8 +88,8 @@ function mostrarCorrecto(e){
 }
 //En caso que lo que haya sido colocado sea incorrecto
 function mostrarError(mensaje, e){
-    e.target.classList.remove('border', 'border-green-500');
-    e.target.classList.add('border', 'border-red-500');
+    coloresInput.verdeDesactivado(e);
+    coloresInput.rojoDesactivado(e);
     const mensajeError = document.createElement('p');
     mensajeError.textContent = mensaje;
     mensajeError.classList.add('border','border-red-500','background-color-100','text-red-500','p-3','error', 'mt-5','text-center');
@@ -88,3 +110,51 @@ function bloquearBoton(activado){
     }
 }
 
+//Enviar mensaje
+function enviarMensaje(e){
+    e.preventDefault();
+    //Se activa el Spinner
+    const spinner = document.querySelector('#spinner')
+    spinner.style.display = 'flex';
+    //Se coloca un tiempo específico para desaparecer el spinner y luego colocar un texto
+    setTimeout(() => {
+        spinner.style.display = 'none';
+        const mensajeEnviado = document.createElement('p');
+        mensajeEnviado.textContent = 'El mensaje ha sido enviado con éxito';
+        mensajeEnviado.classList.add('text-center', 'my-10', 'p-2', 'bg-green-500', 'text-white', 'font-bold', 'uppercase');
+        formulario.insertBefore(mensajeEnviado, spinner);
+
+        setTimeout(() =>{
+            mensajeEnviado.remove();
+            resetearFormulario('AppFinalizada');
+        }, 5000)
+    }, 3000)
+
+    
+}
+
+//Resetear formulario
+function resetearFormulario(e){
+    //Verifica si la app es llamada desde el botón de Reset o porque finalizo la app
+    if (e !== 'AppFinalizada'){
+        e.preventDefault();
+    } 
+    //Reinicia los estilos
+    formulario.reset();
+    resetearEstilos();
+    iniciarApp();
+}
+
+//Resetear los estilos
+
+function resetearEstilos(){
+    const estilosDeError = document.querySelectorAll('input');
+    console.log(estilosDeError)
+    estilosDeError.forEach((elemento)=>{
+        if (elemento.classList.contains('border-red-500')){
+            coloresInput.rojoDesactivado(elemento);
+        } else if (elemento.classList.contains('border-green-500')) {
+            coloresInput.verdeDesactivado(elemento);
+        }
+    })
+}
